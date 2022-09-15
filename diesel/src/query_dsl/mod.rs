6c5b3f3c9913123b4971dca23f394df06a6dde8d
Thomas BESSOU <thomas.bessou@hotmail.fr>
2022-09-15 14:37:26 +0200

    deprecate get_result and load

--- diesel/src/query_dsl/mod.rs
+++ diesel/src/query_dsl/mod.rs
@@ -1543,6 +1543,7 @@ pub trait RunQueryDsl<Conn>: Sized {
     /// #     Ok(())
     /// # }
     /// ```
+    #[deprecated = "Stockly favors using .get_results() instead"]
     fn load<'query, U>(self, conn: &mut Conn) -> QueryResult<Vec<U>>
     where
         Self: LoadQuery<'query, Conn, U>,
@@ -1723,6 +1724,9 @@ pub trait RunQueryDsl<Conn>: Sized {
     /// #     Ok(())
     /// # }
     /// ```
+    #[deprecated = "Use stockly's .get_only_result() if you expect just one line to be returned, \
+            and .first() if you expect several lines to be returned but are only iterested by the \
+            first one"]
     fn get_result<'query, U>(self, conn: &mut Conn) -> QueryResult<U>
     where
         Self: LoadQuery<'query, Conn, U>,
