a4abed9ffdb4a3ea018cbaf01f572e2444999d22
Thomas BESSOU <thomas.bessou@hotmail.fr>
2026-06-01 14:56:54 +0200

    add test that we can't select old ouitside of returning

---
+++ diesel_compile_tests/tests/fail/returning_old_rejected_outside_returning_clause.rs
@@ -0,0 +1,26 @@
+extern crate diesel;
+
+use diesel::pg::returning::old;
+use diesel::prelude::*;
+
+table! {
+    users {
+        id -> Integer,
+        name -> VarChar,
+    }
+}
+
+fn main() {
+    use self::users::dsl::*;
+
+    let mut connection = PgConnection::establish("").unwrap();
+
+    // `old(col)` is only meaningful inside a RETURNING clause.
+    // Using it in a regular SELECT is rejected at compile time.
+    users
+        .select(old(name))
+        //~^ ERROR: cannot select `returning::old_impl::Old<columns::name>` from `users::table`
+        .load::<String>(&mut connection)
+        //~^ ERROR: cannot select `returning::old_impl::Old<columns::name>` from `users::table`
+        .unwrap();
+}
