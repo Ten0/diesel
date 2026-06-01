0457209a88e21505ba3c968f3fd263f38614f994
Thomas BESSOU <thomas.bessou@hotmail.fr>
2026-06-01 19:04:14 +0200

    update compile test

--- diesel_compile_tests/tests/fail/returning_old_rejected_outside_returning_clause.rs
+++ diesel_compile_tests/tests/fail/returning_old_rejected_outside_returning_clause.rs
@@ -28,7 +28,11 @@ fn main() {
     users
         .select(old(name).nullable())
         //~^ ERROR: cannot select `returning::old_impl::Old<columns::name>` from `users::table`
+        //~| ERROR: type mismatch resolving `<table as AppearsInFromClause<OldIdent>>::Count == Once`
+        //~| ERROR: type mismatch resolving `<table as AppearsInFromClause<...>>::Count == Once`
         .load::<Option<String>>(&mut connection)
         //~^ ERROR: cannot select `returning::old_impl::Old<columns::name>` from `users::table`
+        //~| ERROR: type mismatch resolving `<table as AppearsInFromClause<OldIdent>>::Count == Once`
+        //~| ERROR: type mismatch resolving `<table as AppearsInFromClause<...>>::Count == Once`
         .unwrap();
 }
